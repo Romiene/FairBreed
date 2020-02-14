296e06e49daa345fbe0ccbec6b0812caa08c6fff
Gino Siepel <gsiepel@reacollege.eu>
2020-02-14 14:57:12 +0100

    aanmeldingsform beautify en swiftmailer function

---
+++ Design/swiftmailer.php
@@ -0,0 +1,19 @@
+<?php
+  require_once '../vendor/autoload.php';
+  //$logger = new /Swift_Plugins_Loggers_ArrayLogger();
+  function notificationNewSubject($title, $fromMail, $fromName, $toMail, $toName) {
+    // Create the Transport
+    $transport = (new Swift_SmtpTransport('ssl://smtp.gmail.com', 465))
+      ->setUsername('caddymid@gmail.com')
+      ->setPassword('Test42323423!');
+    // Create the Mailer using your created Transport
+    $mailer = new Swift_Mailer($transport);
+    // Create a message
+    $message = (new Swift_Message($title))
+      ->setFrom([$fromMail => $fromName])
+      ->setTo([$toMail => $toName])
+      ->setBody('Here is the message itself');
+    // Send the message
+    $result = $mailer->send($message);
+  }
+?>
